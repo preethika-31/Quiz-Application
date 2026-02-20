187f7462724b3ee5da3fc4f53c4cfa3abce89b3c
preethika-31 <preeethikaharish25@gmail.com>
2026-02-21 01:14:50 +0530

    Fixed SECRET_KEY fallback for development

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i9^p3m1gb$xc^*dmt5wxn*m4cqp9e1#_i4gy3@0^^15$gixh4^'
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
